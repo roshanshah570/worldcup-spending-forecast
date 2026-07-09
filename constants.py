65323f9cfeb6f19c7c94ed928b2d2211028fd41c
Roshan Shah <roshanshah2007@gmail.com>
2026-07-09 00:24:19 -0400

    completed evaluation across all cities, edited sarima model to account for trends, switching to MASE from MAPE as data values close to 0 so blowing up MAPE

---
+++ constants.py
@@ -0,0 +1,3 @@
+target_cities = ['Los Angeles', 'Denver', 'Dallas', 'Chicago', 'Boston', 'Atlanta', 'Kansas City', 'Austin', 'Charlotte']
+host_cities = ['Los Angeles', 'Atlanta', 'Dallas', 'Boston', 'Kansas City']
+nonhost_cities = ['Denver', 'Chicago', 'Austin', 'Charlotte']
